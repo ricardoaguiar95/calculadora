f3ab3ec9c0d61205d7593531906bdf06f3e0fd14
Ricardo <sricardoaguiars@gmail.com>
2022-08-03 22:33:50 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,25 @@
+function insert(num) {
+  var numero = document.getElementById('result').innerHTML
+  document.getElementById('result').innerHTML = numero + num
+}
+
+function clean() {
+  document.getElementById('result').innerHTML = ''
+}
+
+function back() {
+  var result = document.getElementById('result').innerHTML
+  document.getElementById('result').innerHTML = result.substring(
+    0,
+    result.length - 1
+  )
+}
+
+function calcular() {
+  var result = document.getElementById('result').innerHTML
+  if (result) {
+    document.getElementById('result').innerHTML = eval(result)
+  } else {
+    document.getElementById('result').innerHTML = 'Nada...'
+  }
+}
